71864aedea2d3d4406488ba257e28f5f593daae5
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-10-06 16:51:31 -0300

    update

--- src/components/Nav/NavMenu.tsx
+++ src/components/Nav/NavMenu.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./NavMenu.module.scss";
 
@@ -9,26 +8,13 @@ interface NavMenuProps {
 }
 
 export default function NavMenu({ Title, iClass, href }: NavMenuProps) {
-  const [shouldNavigate, setShouldNavigate] = useState(false);
-
-  const handleNavLinkClick = (item:string) => {
-    setShouldNavigate(true);
-    setTimeout(() => {
-      window.location.href = `/${item}`;
-    }, 2000);
-  };
-
-  if (shouldNavigate) {
-    return null; // Não renderize o componente enquanto estiver aguardando a navegação
-  }
-
   return (
     <nav className={styles.navMenu}>
       <ul>
         {href.map((item, index) => {
           return (
             <li key={index}>
-              <NavLink to={`/${item}`} onClick={() => handleNavLinkClick(item)}>
+              <NavLink to={`/${item}`}>
                 <i className={`fa fa-${iClass[index]}`}></i>
                 {Title[index]}
               </NavLink>
