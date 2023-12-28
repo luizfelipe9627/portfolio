e137b39ac5012940dfd0dfdacb3780589549a6cd
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-12-28 12:38:45 -0300

    update

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,70 +1,7 @@
 import styles from "./Projects.module.scss";
-
-// Adicionar os projetos:
-// Obs.: Ainda está faltando 3 projetos que tem pendências.
-import Awax from "../../assets/img/awax.webp";
-import Bikcraft from "../../assets/img/bikcraft.webp";
-import CampinhoBlog from "../../assets/img/campinhoblog.webp";
-import CarrosBicicletas from "../../assets/img/carros-e-bicicletas.webp";
-import Codelandia from "../../assets/img/codelandia.webp";
-import Count from "../../assets/img/count.webp";
-import Disney from "../../assets/img/disney.webp";
-import Fantastika from "../../assets/img/fantastika.webp";
-import Flexblog from "../../assets/img/flexblog.webp";
-import Freshmanstore from "../../assets/img/freshmanstore.webp";
-import GetCoffee from "../../assets/img/getcoffee.webp";
-import Inverno from "../../assets/img/inverno.webp";
-import Jordanshoes from "../../assets/img/jordanshoes.webp";
-import LandingPageM3Academy from "../../assets/img/landing-page-m3-academy.webp";
-import LandingPageM3 from "../../assets/img/landing-page-m3.webp";
-import Pizzaria from "../../assets/img/pizzaria.webp";
-import PlanPage from "../../assets/img/plan-page.webp";
-import PortfolioStartse from "../../assets/img/portfolio-startse.webp";
-import Porfolio2 from "../../assets/img/portfolio2.webp";
-import Quiz from "../../assets/img/quiz.webp";
-import Rocketcoffee from "../../assets/img/rocketcoffee.webp";
-import Selfcare from "../../assets/img/selfcare.webp";
-import Slideshow from "../../assets/img/slideshow.webp";
-import SlideShow2 from "../../assets/img/slideshow2.webp";
-import Starbucks from "../../assets/img/starbucks.webp";
-import Startse from "../../assets/img/startse.webp";
-import TheWitcher from "../../assets/img/the-witcher.webp";
-import ToDoList from "../../assets/img/to-do-list.webp";
-import UrnaEletronica from "../../assets/img/urna-eletronica.webp";
-import ValidadorForm from "../../assets/img/validador-form.webp";
-import Wildbeast from "../../assets/img/wildbeast.webp";
-
-import RiotGames from "../../assets/img/riot-games.webp";
-import TouristAttractions from "../../assets/img/tourist-attractions.webp";
-import M3Academy from "../../assets/img/m3-academy.webp";
-import SuperMario from "../../assets/img/super-mario.webp";
-import SAC from "../../assets/img/sac.webp";
-import Bikcraft2 from "../../assets/img/bikcraft2.webp";
-import Calculadora from "../../assets/img/calculadora.webp";
-import AnimaisFantasticos from "../../assets/img/animais-fantasticos.webp";
-import Steam from "../../assets/img/steam.webp";
-import Ranek from "../../assets/img/ranek.webp";
-import Dogs from "../../assets/img/dogs.webp";
-import Naped from "../../assets/img/naped.webp";
-import Blogames from "../../assets/img/blogames.webp";
-import Calculadora2 from "../../assets/img/calculadora2.webp";
-import FormPesquisa from "../../assets/img/formulario-pesquisa.webp";
-import Dados from "../../assets/img/dados.webp";
-import SlideStories from "../../assets/img/slide-stories.webp";
-
-import html from "../../assets/svg/html.svg";
-import css from "../../assets/svg/css.svg";
-import javascript from "../../assets/svg/javascript.svg";
-import react from "../../assets/svg/react.svg";
-import typescript from "../../assets/svg/typescript.svg";
-import sass from "../../assets/svg/sass.svg";
-import webpack from "../../assets/svg/webpack.svg";
-import gulp from "../../assets/svg/gulp.svg";
-import eslint from "../../assets/svg/eslint.svg";
-import styledComponents from "../../assets/svg/styled-components.svg";
-
 import ProjectItem from "../../components/ProjectItem/ProjectItem";
 import Title from "../../components/Title/Title";
+import projectsSorted from "./ProjectsData";
 
 export default function Projects() {
   return (
@@ -73,129 +10,20 @@ export default function Projects() {
         <Title title="Projetos" />
 
         <div className={styles.heading}>
-          <h2>Meus últimos projetos :</h2>
+          <h2>Meus projetos :</h2>
         </div>
 
         <div className={styles.row}>
-          <ProjectItem
-            img={SlideStories}
-            alt="Slide Stories"
-            href="https://luizfelipe9627-slide-stories-origamid.netlify.app"
-            skill={["HTML", "CSS", "TypeScript"]}
-            icon={[html, css, typescript]}
-          />
-          <ProjectItem
-            img={Dados}
-            alt="Dados"
-            href="https://luizfelipe9627-dados-origamid.netlify.app"
-            skill={["HTML", "CSS", "TypeScript"]}
-            icon={[html, css, typescript]}
-          />
-          <ProjectItem
-            img={FormPesquisa}
-            alt="Formulário de Pesquisa"
-            href="https://luizfelipe9627-formulario-pesquisa.netlify.app"
-            skill={["HTML", "CSS", "TypeScript"]}
-            icon={[html, css, typescript]}
-          />
-          <ProjectItem
-            img={Calculadora2}
-            alt="Calculadora"
-            href="https://luizfelipe9627-calculadora-vnw.netlify.app"
-            skill={["React.JS", "Styled Components", "Eslint"]}
-            icon={[react, styledComponents, eslint]}
-          />
-          <ProjectItem
-            img={Blogames}
-            alt="Blogames"
-            href="https://luizfelipe9627-blogames.netlify.app"
-            skill={["React.JS", "Styled Components", "Eslint"]}
-            icon={[react, styledComponents, eslint]}
-          />
-          <ProjectItem
-            img={Naped}
-            alt="Naped"
-            href="https://luizfelipe9627-naped.netlify.app"
-            skill={["React.JS", "CSS3", "Eslint"]}
-            icon={[react, css, eslint]}
-          />
-          <ProjectItem
-            img={Dogs}
-            alt="Dogs"
-            href="https://luizfelipe9627-dogs.netlify.app"
-            skill={["React.JS", "CSS3", "Eslint"]}
-            icon={[react, css, eslint]}
-          />
-          <ProjectItem
-            img={Ranek}
-            alt="Ranek"
-            href="https://luizfelipe9627-ranek.netlify.app"
-            skill={["React.JS", "CSS3", "Eslint"]}
-            icon={[react, css, eslint]}
-          />
-          <ProjectItem
-            img={Steam}
-            alt="Steam"
-            href="https://luizfelipe9627-steam-redesign.netlify.app"
-            skill={["HTML5", "CSS3", "JavaScript", "Webpack", "Eslint"]}
-            icon={[html, css, javascript, webpack, eslint]}
-          />
-          <ProjectItem
-            img={AnimaisFantasticos}
-            alt="Animais Fantásticos"
-            href="https://luizfelipe9627-animais-fantasticos.netlify.app"
-            skill={["HTML5", "CSS3", "JavaScript", "Webpack", "Eslint"]}
-            icon={[html, css, javascript, webpack, eslint]}
-          />
-          <ProjectItem
-            img={Calculadora}
-            alt="Calculadora"
-            href="https://luizfelipe9627-calculadora.netlify.app"
-            skill={["HTML5", "CSS3", "JavaScript", "Webpack", "Eslint"]}
-            icon={[html, css, javascript, webpack, eslint]}
-          />
-          <ProjectItem
-            img={Bikcraft2}
-            alt="Bikcraft"
-            href="https://luizfelipe9627-bikcraft-2.netlify.app"
-            skill={["HTML5", "CSS3", "JavaScript"]}
-            icon={[html, css, javascript]}
-          />
-          <ProjectItem
-            img={SAC}
-            alt="SAC"
-            href="https://luizfelipe9627-simulador-sac.netlify.app"
-            skill={["HTML5", "CSS3", "JavaScript"]}
-            icon={[html, css, javascript]}
-          />
-          <ProjectItem
-            img={SuperMario}
-            alt="Super Mario"
-            href="https://luizfelipe9627-super-mario.netlify.app"
-            skill={["HTML5", "CSS3", "JavaScript"]}
-            icon={[html, css, javascript]}
-          />
-          <ProjectItem
-            img={M3Academy}
-            alt="M3 Academy"
-            href="https://luizfelipe9627-react-e-typescript-m3.netlify.app"
-            skill={["React.JS", "TypeScript", "SASS", "Eslint"]}
-            icon={[react, typescript, sass, eslint]}
-          />
-          <ProjectItem
-            img={TouristAttractions}
-            alt="Tourist Attractions"
-            href="https://luizfelipe9627-tourist-attractions-m3.netlify.app"
-            skill={["HTML5", "SASS", "JavaScript", "Gulp"]}
-            icon={[html, sass, javascript, gulp]}
-          />
-          <ProjectItem
-            img={RiotGames}
-            alt="Riot Games"
-            href="https://luizfelipe9627-login-lol.netlify.app"
-            skill={["HTML5", "CSS3", "JavaScript"]}
-            icon={[html, css, javascript]}
-          />
+          {projectsSorted.map((project, index) => (
+            <ProjectItem
+              key={index}
+              img={project.img}
+              alt={project.alt}
+              href={project.href}
+              skill={project.skill}
+              icon={project.icon}
+            />
+          ))}
         </div>
       </div>
     </section>
---
+++ src/pages/Projects/ProjectsData.tsx
@@ -0,0 +1,456 @@
+// Adicionar os projetos:
+// Obs.: Ainda está faltando 3 projetos que tem pendências.
+import Awax from "../../assets/img/awax.webp";
+import Bikcraft from "../../assets/img/bikcraft.webp";
+import CampinhoBlog from "../../assets/img/campinhoblog.webp";
+import CarrosBicicletas from "../../assets/img/carros-e-bicicletas.webp";
+import Codelandia from "../../assets/img/codelandia.webp";
+import Count from "../../assets/img/count.webp";
+import Disney from "../../assets/img/disney.webp";
+import Fantastika from "../../assets/img/fantastika.webp";
+import Flexblog from "../../assets/img/flexblog.webp";
+import Freshmanstore from "../../assets/img/freshmanstore.webp";
+import GetCoffee from "../../assets/img/getcoffee.webp";
+import Inverno from "../../assets/img/inverno.webp";
+import Jordanshoes from "../../assets/img/jordanshoes.webp";
+import LandingPageM3Academy from "../../assets/img/landing-page-m3-academy.webp";
+import ClassLandingPageM3 from "../../assets/img/class-landing-page-m3.webp";
+import Pizzaria from "../../assets/img/pizzaria.webp";
+import PlanPage from "../../assets/img/plan-page.webp";
+import PortfolioStartse from "../../assets/img/portfolio-startse.webp";
+import PorfolioOrigamid from "../../assets/img/portfolio-origamid.webp";
+import Quiz from "../../assets/img/quiz.webp";
+import Rocketcoffee from "../../assets/img/rocketcoffee.webp";
+import Selfcare from "../../assets/img/selfcare.webp";
+import SlideshowB7Web from "../../assets/img/slideshow-b7web.webp";
+import SlideShowOrigamid from "../../assets/img/slideshow-origamid.webp";
+import Starbucks from "../../assets/img/starbucks.webp";
+import Startse from "../../assets/img/startse.webp";
+import TheWitcher from "../../assets/img/the-witcher.webp";
+import ToDoList from "../../assets/img/to-do-list.webp";
+import UrnaEletronica from "../../assets/img/urna-eletronica.webp";
+import ValidadorForm from "../../assets/img/validador-form.webp";
+import Wildbeast from "../../assets/img/wildbeast.webp";
+import RiotGames from "../../assets/img/riot-games.webp";
+import TouristAttractionsM3 from "../../assets/img/tourist-attractions-m3.webp";
+import M3Academy from "../../assets/img/m3-academy.webp";
+import SuperMario from "../../assets/img/super-mario.webp";
+import SAC from "../../assets/img/sac.webp";
+import Bikcraft2 from "../../assets/img/bikcraft2.webp";
+import Calculadora from "../../assets/img/calculadora.webp";
+import AnimaisFantasticos from "../../assets/img/animais-fantasticos.webp";
+import Steam from "../../assets/img/steam.webp";
+import Ranek from "../../assets/img/ranek.webp";
+import Dogs from "../../assets/img/dogs.webp";
+import Naped from "../../assets/img/naped.webp";
+import Blogames from "../../assets/img/blogames.webp";
+import CalculadoraRocketseat from "../../assets/img/calculadora-rocketseat.webp";
+import FormPesquisa from "../../assets/img/formulario-pesquisa.webp";
+import Dados from "../../assets/img/dados.webp";
+import SlideStories from "../../assets/img/slide-stories.webp";
+import VideoPlayerOrigamid from "../../assets/img/video-player-origamid.webp";
+import Fintech from "../../assets/img/fintech.webp";
+import MyPortfolio from "../../assets/img/my-portfolio.webp";
+import RickAndMorty from "../../assets/img/rick-and-morty.webp";
+import DTMoney from "../../assets/img/dt-money.webp";
+
+import html from "../../assets/svg/html.svg";
+import css from "../../assets/svg/css.svg";
+import javascript from "../../assets/svg/javascript.svg";
+import react from "../../assets/svg/react.svg";
+import typescript from "../../assets/svg/typescript.svg";
+import sass from "../../assets/svg/sass.svg";
+import webpack from "../../assets/svg/webpack.svg";
+import gulp from "../../assets/svg/gulp.svg";
+import vite from "../../assets/svg/vite.svg";
+import eslint from "../../assets/svg/eslint.svg";
+import styledComponents from "../../assets/svg/styled-components.svg";
+import api from "../../assets/svg/api.svg";
+
+const projects = [
+  {
+    img: Awax,
+    alt: "Awax",
+    href: "https://luizfelipe9627-awax.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: Bikcraft,
+    alt: "Bikcraft",
+    href: "https://luizfelipe9627-bikcraft.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: CampinhoBlog,
+    alt: "Campinho Blog",
+    href: "https://campinhoblog.netlify.app",
+    skill: ["React.JS", "Vite.JS", "CSS3", "API Rest"],
+    icon: [react, vite, css, api],
+  },
+  {
+    img: CarrosBicicletas,
+    alt: "Carros e Bicicletas",
+    href: "https://luizfelipe9627-carros-e-bicicletas.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: Codelandia,
+    alt: "Codelandia",
+    href: "https://luizfelipe9627-codelandia.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: Count,
+    alt: "Count",
+    href: "https://luizfelipe9627-count.netlify.app",
+    skill: ["React.JS", "Vite.JS", "CSS3"],
+    icon: [react, vite, css],
+  },
+  {
+    img: Disney,
+    alt: "Disney",
+    href: "https://luizfelipe9627-disney.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: Fantastika,
+    alt: "Fantastika",
+    href: "https://luizfelipe9627-fantastika.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: Flexblog,
+    alt: "Flexblog",
+    href: "https://luizfelipe9627-flexblog.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: Freshmanstore,
+    alt: "Freshmanstore",
+    href: "https://luizfelipe9627-freshmanstore.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: GetCoffee,
+    alt: "GetCoffee",
+    href: "https://luizfelipe9627-getcoffee.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: Inverno,
+    alt: "Inverno",
+    href: "https://luizfelipe9627-inverno.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: Jordanshoes,
+    alt: "Jordanshoes",
+    href: "https://luizfelipe9627-jordanshoes.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+
+  {
+    img: LandingPageM3Academy,
+    alt: "Landing Page M3 Academy",
+    href: "https://luizfelipe9627-landing-page-m3.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: ClassLandingPageM3,
+    alt: "Class Landing Page M3",
+    href: "https://luizfelipe9627-class-landing-page-m3.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: Pizzaria,
+    alt: "Pizzaria",
+    href: "https://luizfelipe9627-pizzaria.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: PlanPage,
+    alt: "Plan Page",
+    href: "https://luizfelipe9627-plan-page.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: PortfolioStartse,
+    alt: "Portfolio Startse",
+    href: "https://luizfelipe9627-portfolio-startse.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: PorfolioOrigamid,
+    alt: "Porfolio Origamid",
+    href: "https://luizfelipe9627-portfolio-origamid.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: Quiz,
+    alt: "Quiz Origamid",
+    href: "https://luizfelipe9627-quiz-origamid.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [react, css],
+  },
+  {
+    img: Rocketcoffee,
+    alt: "Rocketcoffee",
+    href: "https://luizfelipe9627-rocketcoffee.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: Selfcare,
+    alt: "Selfcare",
+    href: "https://luizfelipe9627-selfcare.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: SlideshowB7Web,
+    alt: "Slideshow B7Web",
+    href: "https://luizfelipe9627-slideshow-b7web.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: SlideShowOrigamid,
+    alt: "Slideshow Origamid",
+    href: "https://luizfelipe9627-slideshow-origamid.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: Starbucks,
+    alt: "Starbucks",
+    href: "https://luizfelipe9627-starbucks.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: Startse,
+    alt: "Startse",
+    href: "https://luizfelipe9627-startse.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: TheWitcher,
+    alt: "The Witcher",
+    href: "https://luizfelipe9627-the-witcher.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: ToDoList,
+    alt: "To Do List",
+    href: "https://luizfelipe9627-to-do-list.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: UrnaEletronica,
+    alt: "Urna Eletrônica",
+    href: "https://luizfelipe9627-urna-eletronica.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: ValidadorForm,
+    alt: "Validador Form",
+    href: "https://luizfelipe9627-validador-form.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: Wildbeast,
+    alt: "Wildbeast",
+    href: "https://luizfelipe9627-wildbeast.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+
+  {
+    img: RiotGames,
+    alt: "Riot Games",
+    href: "https://luizfelipe9627-login-lol.netlify.app",
+    skill: ["HTML5", "CSS3"],
+    icon: [html, css],
+  },
+  {
+    img: Bikcraft2,
+    alt: "Bikcraft 2",
+    href: "https://luizfelipe9627-bikcraft2.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: Calculadora,
+    alt: "Calculadora",
+    href: "https://luizfelipe9627-calculadora.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript", "Webpack", "Eslint"],
+    icon: [html, css, javascript, webpack, eslint],
+  },
+  {
+    img: AnimaisFantasticos,
+    alt: "Animais Fantásticos",
+    href: "https://luizfelipe9627-animais-fantasticos.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript", "Webpack", "Eslint"],
+    icon: [html, css, javascript, webpack, eslint],
+  },
+  {
+    img: Steam,
+    alt: "Steam",
+    href: "https://luizfelipe9627-steam-redesign.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript", "Webpack", "Eslint"],
+    icon: [html, css, javascript, webpack, eslint],
+  },
+  {
+    img: Ranek,
+    alt: "Ranek",
+    href: "https://luizfelipe9627-ranek.netlify.app",
+    skill: ["React.JS", "CSS3", "Eslint", "API Rest"],
+    icon: [react, css, eslint, api],
+  },
+  {
+    img: Dogs,
+    alt: "Dogs",
+    href: "https://luizfelipe9627-dogs.netlify.app",
+    skill: ["React.JS", "CSS3", "Eslint", "API Rest"],
+    icon: [react, css, eslint, api],
+  },
+  {
+    img: Naped,
+    alt: "Naped",
+    href: "https://luizfelipe9627-naped.netlify.app",
+    skill: ["React.JS", "CSS3", "Eslint"],
+    icon: [react, css, eslint],
+  },
+  {
+    img: Blogames,
+    alt: "Blogames",
+    href: "https://luizfelipe9627-blogames.netlify.app",
+    skill: ["React.JS", "Styled Components", "Eslint"],
+    icon: [react, styledComponents, eslint],
+  },
+  {
+    img: CalculadoraRocketseat,
+    alt: "Calculadora Rocketseat",
+    href: "https://luizfelipe9627-calculadora-rocketseat.netlify.app",
+    skill: ["React.JS", "Styled Components", "Eslint"],
+    icon: [react, styledComponents, eslint],
+  },
+  {
+    img: FormPesquisa,
+    alt: "Formulário de Pesquisa",
+    href: "https://luizfelipe9627-formulario-pesquisa.netlify.app",
+    skill: ["HTML", "CSS3", "TypeScript"],
+    icon: [html, css, typescript],
+  },
+  {
+    img: Dados,
+    alt: "Dados",
+    href: "https://luizfelipe9627-dados-origamid.netlify.app",
+    skill: ["HTML", "CSS3", "TypeScript", "API Rest"],
+    icon: [html, css, typescript, api],
+  },
+  {
+    img: SlideStories,
+    alt: "Slide Stories",
+    href: "https://luizfelipe9627-slide-stories-origamid.netlify.app",
+    skill: ["HTML", "CSS3", "TypeScript"],
+    icon: [html, css, typescript],
+  },
+  {
+    img: TouristAttractionsM3,
+    alt: "Tourist Attractions M3",
+    href: "https://luizfelipe9627-tourist-attractions-m3.netlify.app",
+    skill: ["HTML5", "SASS", "JavaScript", "Gulp"],
+    icon: [html, sass, javascript, gulp],
+  },
+  {
+    img: M3Academy,
+    alt: "M3 Academy",
+    href: "https://luizfelipe9627-react-e-typescript-m3.netlify.app",
+    skill: ["React.JS", "TypeScript", "SASS", "Eslint"],
+    icon: [react, typescript, sass, eslint],
+  },
+  {
+    img: SuperMario,
+    alt: "Super Mario",
+    href: "https://luizfelipe9627-super-mario.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: SAC,
+    alt: "SAC",
+    href: "https://luizfelipe9627-simulador-sac.netlify.app",
+    skill: ["HTML5", "CSS3", "JavaScript"],
+    icon: [html, css, javascript],
+  },
+  {
+    img: VideoPlayerOrigamid,
+    alt: "Video Player Origamid",
+    href: "https://luizfelipe9627-video-player-origamid.netlify.app",
+    skill: ["React.JS", "Vite.JS", "CSS3", "Eslint"],
+    icon: [react, vite, css, eslint],
+  },
+  {
+    img: MyPortfolio,
+    alt: "My Portfolio",
+    href: "https://luizfelipe9627-my-portfolio.netlify.app",
+    skill: ["React.JS", "Vite.JS", "SASS", "Eslint"],
+    icon: [react, vite, sass, eslint],
+  },
+  {
+    img: Fintech,
+    alt: "Fintech",
+    href: "https://luizfelipe9627-fintech.netlify.app",
+    skill: ["React.JS", "Vite.JS", "TypeScript", "CSS3", "Eslint", "API Rest"],
+    icon: [react, vite, typescript, css, eslint, api],
+  },
+  {
+    img: RickAndMorty,
+    alt: "Rick e Morty",
+    href: "https://luizfelipe9627-rick-e-morty.netlify.app",
+    skill: ["React.JS", "Vite.JS", "TypeScript", "SASS", "Eslint", "API Rest"],
+    icon: [react, vite, typescript, sass, eslint, api],
+  },
+  {
+    img: DTMoney,
+    alt: "DT Money",
+    href: "https://luizfelipe9627-dt-money.netlify.app",
+    skill: ["React.JS", "Vite.JS", "TypeScript", "SASS", "Eslint"],
+    icon: [react, vite, typescript, sass, eslint],
+  },
+];
+
+const projectsSorted = projects.sort((a, b) => {
+  if (a.alt < b.alt) {
+    return -1;
+  }
+  if (a.alt > b.alt) {
+    return 1;
+  }
+  return 0;
+});
+
+export default projectsSorted;
